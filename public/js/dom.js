dd6f75e230e8d5041f458db6f4fc077d4361068b
jimmy706 <jim.dangquoc706@gmail.com>
2019-01-22 22:08:42 +0700

    dfrgdf

--- public/js/dom.js
+++ public/js/dom.js
@@ -10,10 +10,27 @@ window.onload = function () {
     let pageList = document.querySelector("#page-list");
     if (pageList) {
         const locate = getUrlParameter("page") || 1;
-        const links = pageList.querySelectorAll("[data-locate]");
+        const links = [...pageList.querySelectorAll("[data-locate]")];
+        const prev = pageList.querySelector(".page-prev");
+        const next = pageList.querySelector(".page-next");
+        const prevLink = prev.querySelector(".page-link");
+        const nextLink = next.querySelector(".page-link");
+        const prevLocate = parseInt(locate) - 1;
+        const nextLocate = parseInt(locate) + 1;
+
+        // set link for next and prev 
+        prevLink.href = `/products?page=${prevLocate}`;
+        nextLink.href = `/products?page=${nextLocate}`;
+        
+        // add disabled class if page at edge
+        if(links.length == locate)
+            next.classList.add("disabled");
+        else if(locate == 1)
+            prev.classList.add("disabled");
+        
 
         links.forEach(link => {
-            if(link.dataset.locate === locate){
+            if(link.dataset.locate == locate){
                 link.classList.add("active");
             }
         })
